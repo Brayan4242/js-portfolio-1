48ba8b7964c729f07ebe10412b8529b78024b724
Brayan4242 <dreyparker493@gmail.com>
2021-09-06 17:55:36 -0500

    clase 10

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
                     'css-loader',
                     'stylus-loader'
                 ],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
             }
         ]
     },
